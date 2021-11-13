7b034d22a8cdbba85d7cb12d44e86b226560e6f5
Dipansh Parmar <dipanshparmar870@gmail.com>
2021-11-13 10:15:04 +0530

    added ontap

--- lib/widgets/custom_container.dart
+++ lib/widgets/custom_container.dart
@@ -5,32 +5,37 @@ class CustomContainer extends StatelessWidget {
     this._text, {
     Key? key,
     this.normalWeight = false,
+    this.onTap,
   }) : super(key: key);
 
   final String _text;
   final bool normalWeight;
+  final Function()? onTap;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(20),
-      margin: const EdgeInsets.only(
-        top: 20,
-        right: 20,
-        left: 20,
-      ),
-      decoration: BoxDecoration(
-        color: const Color(0xFF616161),
-        borderRadius: BorderRadius.circular(10),
-      ),
-      child: Center(
-        child: Text(
-          _text,
-          maxLines: 1,
-          overflow: TextOverflow.ellipsis,
-          style: TextStyle(
-            color: Colors.white,
-            fontWeight: normalWeight ? FontWeight.normal : FontWeight.bold,
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        padding: const EdgeInsets.all(20),
+        margin: const EdgeInsets.only(
+          top: 20,
+          right: 20,
+          left: 20,
+        ),
+        decoration: BoxDecoration(
+          color: const Color(0xFF616161),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Center(
+          child: Text(
+            _text,
+            maxLines: 1,
+            overflow: TextOverflow.ellipsis,
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: normalWeight ? FontWeight.normal : FontWeight.bold,
+            ),
           ),
         ),
       ),
