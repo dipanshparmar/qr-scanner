28c5df414dcf4e35d83550a552b8ae676723f49d
Dipansh Parmar <dipanshparmar870@gmail.com>
2021-11-14 15:21:48 +0530

    fixed flash and camera not working

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:qr_code_scanner/qr_code_scanner.dart';
@@ -15,6 +17,9 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   // key for the qr scanner
   final GlobalKey _qrKey = GlobalKey(debugLabel: 'QR');
+
+  // flash icon
+  IconData _flashIcon = Icons.flashlight_off;
 
   // controller for the scanner
   QRViewController? _qrViewController;
@@ -83,9 +88,82 @@ class _HomePageState extends State<HomePage> {
             },
           ),
           // flash and camera controls
-          FlashAndCameraControls(_qrViewController)
+          SafeArea(
+            // this container is for alignment
+            child: Container(
+              width: double.infinity,
+              alignment: Alignment.topCenter,
+              margin: const EdgeInsets.only(top: 10),
+              // this container is for decoration
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Theme.of(context).primaryColor,
+                ),
+                child: _buildButtons(),
+              ),
+            ),
+          ),
         ],
       ),
     );
+  }
+
+  // method to build the buttons
+  Widget _buildButtons() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        IconButton(
+          icon: Icon(_flashIcon),
+          color: Colors.white,
+          // toggle the flash on click
+          onPressed: () async => _toggleFlash(),
+        ),
+        const SizedBox(
+          width: 10,
+        ),
+        IconButton(
+          icon: const Icon(Icons.flip_camera_android),
+          color: Colors.white,
+          onPressed: () async => _toggleCamera(),
+        )
+      ],
+    );
+  }
+
+  // method to toggle flash
+  Future<void> _toggleFlash() async {
+    // if controller is not null
+    if (_qrViewController != null) {
+      // toggle the flash
+      await _qrViewController!.toggleFlash();
+
+      // getting the status of the flash
+      final bool? isOn = await _qrViewController!.getFlashStatus();
+
+      // updating the icon
+      if (isOn != null) {
+        if (isOn) {
+          setState(() {
+            _flashIcon = Icons.flashlight_on;
+          });
+        } else {
+          setState(() {
+            _flashIcon = Icons.flashlight_off;
+          });
+        }
+      }
+    }
+  }
+
+  // method to toggle the camera
+  Future<void> _toggleCamera() async {
+    // if controller is not null
+    if (_qrViewController != null) {
+      // flip the camera
+      await _qrViewController!.flipCamera();
+    }
   }
 }
--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -1,4 +1,3 @@
 export './custom_container.dart';
-export './flash_and_camera_controls.dart';
 export './options_modal_sheet.dart';
 export './saved_scan_tile.dart';
