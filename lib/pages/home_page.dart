a650ff672ce356e526e9d399e7c26276dacd4072
Dipansh Parmar <dipanshparmar870@gmail.com>
2021-11-15 17:51:10 +0530

    refactor

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -34,76 +34,74 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        children: [
-          QRView(
-            key: _qrKey,
-            overlay: QrScannerOverlayShape(
-              borderColor: Colors.white,
-              borderRadius: 10,
-              borderLength: 20,
-              borderWidth: 8,
-            ),
-            onQRViewCreated: (controller) {
-              // assigning the controller
-              _qrViewController = controller;
+    return Stack(
+      children: [
+        QRView(
+          key: _qrKey,
+          overlay: QrScannerOverlayShape(
+            borderColor: Colors.white,
+            borderRadius: 10,
+            borderLength: 20,
+            borderWidth: 8,
+          ),
+          onQRViewCreated: (controller) {
+            // assigning the controller
+            _qrViewController = controller;
 
-              // listening for detection
-              _qrViewController!.scannedDataStream.listen(
-                (barcode) async {
-                  // pause the scanning
-                  await _qrViewController!.pauseCamera();
+            // listening for detection
+            _qrViewController!.scannedDataStream.listen(
+              (barcode) async {
+                // pause the scanning
+                await _qrViewController!.pauseCamera();
 
-                  // give a feedback to let the user know that there is a detection
-                  HapticFeedback.heavyImpact();
+                // give a feedback to let the user know that there is a detection
+                HapticFeedback.heavyImpact();
 
-                  // getting the date when the qr is captured
-                  final date = DateTime.now().toString();
+                // getting the date when the qr is captured
+                final date = DateTime.now().toString();
 
-                  // opening the modal bottom sheet for different options
-                  await showModalBottomSheet(
-                    shape: const RoundedRectangleBorder(
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(10),
-                        topRight: Radius.circular(10),
-                      ),
+                // opening the modal bottom sheet for different options
+                await showModalBottomSheet(
+                  shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(10),
+                      topRight: Radius.circular(10),
                     ),
-                    backgroundColor: Theme.of(context).primaryColor,
-                    context: context,
-                    builder: (context) {
-                      return OptionsModalSheet(
-                        barcode,
-                        date: date,
-                      );
-                    },
-                  );
+                  ),
+                  backgroundColor: Theme.of(context).primaryColor,
+                  context: context,
+                  builder: (context) {
+                    return OptionsModalSheet(
+                      barcode,
+                      date: date,
+                    );
+                  },
+                );
 
-                  // once the sheet is closed resume the camera again
-                  await _qrViewController!.resumeCamera();
-                },
-              );
-            },
-          ),
-          // flash and camera controls
-          SafeArea(
-            // this container is for alignment
+                // once the sheet is closed resume the camera again
+                await _qrViewController!.resumeCamera();
+              },
+            );
+          },
+        ),
+        // flash and camera controls
+        SafeArea(
+          // this container is for alignment
+          child: Container(
+            width: double.infinity,
+            alignment: Alignment.topCenter,
+            margin: const EdgeInsets.only(top: 10),
+            // this container is for decoration
             child: Container(
-              width: double.infinity,
-              alignment: Alignment.topCenter,
-              margin: const EdgeInsets.only(top: 10),
-              // this container is for decoration
-              child: Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  color: Theme.of(context).primaryColor,
-                ),
-                child: _buildButtons(),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: Theme.of(context).primaryColor,
               ),
+              child: _buildButtons(),
             ),
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 
